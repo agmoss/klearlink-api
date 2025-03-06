010d113fb40fb0a3aa6decf5290098cb32248234
Andrew Moss <andrew@m0ss.dev>
2025-03-06 13:05:29 -0700

    feat: Adding in better linting and file check

--- migrations/2025-02-23-160550_seed-user/up.sql
+++ migrations/2025-02-23-160550_seed-user/up.sql
@@ -3,5 +3,9 @@
 DELETE FROM users WHERE username = 'test_admin_user';
 INSERT INTO users (username, api_key, role, created_at, updated_at)
 VALUES (
-    'test_admin_user', 'c491a813-234a-4bea-b6c4-7413b244dea4', 'admin', NOW(), NOW()
+    'test_admin_user',
+    'c491a813-234a-4bea-b6c4-7413b244dea4',
+    'admin',
+    NOW(),
+    NOW()
 );
